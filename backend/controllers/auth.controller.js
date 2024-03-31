02deec488886a2d20f3dc2285d1a293497b3cd68
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-03-31 20:57:39 +0200

    add routes and file structure for signup, login, logout

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signUp = (req, res) => {
+  res.send("sinupuser");
+  console.log("signUpUser");
+};
+
+export const login = (req, res) => {
+  console.log("loginUser");
+};
+
+export const logout = (req, res) => {
+  console.log("logoutUser");
+};
